f824a9d78ba6a29c18c9a1a9f5df27d866d47a10
AleksandrHovhannisyan <aleksandrhovhannisyan@gmail.com>
2019-11-02 13:36:43 -0400

    Load incident characteristics dynamically from DB

--- client/src/pages/CatalogPage/DeepDive/IncidentForm.tsx
+++ client/src/pages/CatalogPage/DeepDive/IncidentForm.tsx
@@ -1,15 +1,13 @@
 import React, { ChangeEvent } from 'react';
-import { Select } from 'antd';
-import { incidentCharacteristics } from 'mockData/MockData';
 import DataForm from './DataForm/DataForm';
 import EqualityInput from 'components/Forms/EqualityInput/EqualityInput';
-import { DatePicker } from 'antd';
+import { DatePicker, Spin } from 'antd';
 import { RangePickerValue } from 'antd/lib/date-picker/interface';
 import moment from 'moment';
 import FormField from 'components/Forms/FormField/FormField';
 import MultiSelect from 'components/Forms/MultiSelect/MultiSelect';
+import axios from 'axios';
 
-const { Option } = Select;
 const { RangePicker } = DatePicker;
 
 export const earliestDate = '01/01/2013';
@@ -33,46 +31,86 @@ interface IncidentFormProps {
   ) => void;
 }
 
-const IncidentForm = (props: IncidentFormProps) => {
-  return (
-    <DataForm>
-      <h2>Crime Characteristics</h2>
+interface IncidentFormState {
+  waitingForIncidentData: boolean;
+  incidentCharacteristics: string[];
+}
 
-      <FormField label="Incident type">
-        <MultiSelect
-          style={{ minWidth: '200px' }}
-          placeholder="Select incident types"
-          data={incidentCharacteristics}
-          onChange={props.onCharacteristicChange}
-        />
-      </FormField>
+class IncidentForm extends React.Component<
+  IncidentFormProps,
+  IncidentFormState
+> {
+  public constructor(props: IncidentFormProps) {
+    super(props);
+    this.state = {
+      waitingForIncidentData: true,
+      incidentCharacteristics: [],
+    };
+    this.getIncidentCharacteristics();
+  }
 
-      <FormField label="Number killed">
-        <EqualityInput
-          numericalMinimum={0}
-          onEqualityChange={props.onKilledEqualityChange}
-          onNumberChange={props.onKillCountChange}
-        />
-      </FormField>
+  private getIncidentCharacteristics = async () => {
+    try {
+      const response = await axios.get('/api/incident/characteristics');
 
-      <FormField label="Number injured">
-        <EqualityInput
-          numericalMinimum={0}
-          onEqualityChange={props.onInjuredEqualityChange}
-          onNumberChange={props.onInjuredCountChange}
-        />
-      </FormField>
+      const characteristics: string[] = [];
+      response.data.forEach((c: { INCIDENT_CHARACTERISTIC: string }) =>
+        characteristics.push(c.INCIDENT_CHARACTERISTIC)
+      );
 
-      <FormField label="Time range">
-        <RangePicker
-          onChange={props.onDateRangeChange}
-          format="MM/DD/YYYY"
-          defaultPickerValue={[moment(earliestDate), moment(latestDate)]}
-          disabledDate={forbiddenDates}
-        />
-      </FormField>
-    </DataForm>
-  );
-};
+      this.setState({
+        waitingForIncidentData: false,
+        incidentCharacteristics: characteristics,
+      });
+    } catch (error) {}
+  };
+
+  public render() {
+    return (
+      <DataForm>
+        <h2>Crime Characteristics</h2>
+
+        <FormField label="Incident type">
+          <MultiSelect
+            style={{ minWidth: '200px' }}
+            placeholder={
+              this.state.waitingForIncidentData
+                ? 'Loading data...'
+                : 'Select incident types'
+            }
+            data={this.state.incidentCharacteristics}
+            disabled={this.state.waitingForIncidentData}
+            onChange={this.props.onCharacteristicChange}
+          />
+        </FormField>
+
+        <FormField label="Number killed">
+          <EqualityInput
+            numericalMinimum={0}
+            onEqualityChange={this.props.onKilledEqualityChange}
+            onNumberChange={this.props.onKillCountChange}
+          />
+        </FormField>
+
+        <FormField label="Number injured">
+          <EqualityInput
+            numericalMinimum={0}
+            onEqualityChange={this.props.onInjuredEqualityChange}
+            onNumberChange={this.props.onInjuredCountChange}
+          />
+        </FormField>
+
+        <FormField label="Time range">
+          <RangePicker
+            onChange={this.props.onDateRangeChange}
+            format="MM/DD/YYYY"
+            defaultPickerValue={[moment(earliestDate), moment(latestDate)]}
+            disabledDate={forbiddenDates}
+          />
+        </FormField>
+      </DataForm>
+    );
+  }
+}
 
 export default IncidentForm;
