042f9b02ceec2c1f9cffac1a3c8ff1680519e4df
AleksandrHovhannisyan <aleksandrhovhannisyan@gmail.com>
2019-11-02 13:36:28 -0400

    Add new routes for fetching form data

---
+++ server/src/routes/Location.ts
@@ -0,0 +1,75 @@
+import { logger } from '@shared';
+import { Request, Response, Router } from 'express';
+import { BAD_REQUEST, OK } from 'http-status-codes';
+import query from 'src/query/query';
+
+// Init shared
+const router = Router();
+
+/**
+ * Returns all states in which gun crimes can occur.
+ */
+router.get('/states', async (req: Request, res: Response) => {
+  try {
+    const states = await query(`SELECT DISTINCT state FROM Location`);
+    return res.status(OK).json(states);
+  } catch (err) {
+    logger.error(err.message, err);
+    return res.status(BAD_REQUEST).json({
+      error: err.message,
+    });
+  }
+});
+
+/**
+ * Returns all cities/counties in which gun crimes can occur.
+ */
+router.get('/citiesOrCounties', async (req: Request, res: Response) => {
+  try {
+    const citiesOrCounties = await query(
+      `SELECT DISTINCT city_or_county FROM Location`
+    );
+    return res.status(OK).json(citiesOrCounties);
+  } catch (err) {
+    logger.error(err.message, err);
+    return res.status(BAD_REQUEST).json({
+      error: err.message,
+    });
+  }
+});
+
+/**
+ * Returns all house districts in which gun crimes can occur.
+ */
+router.get('/houseDistricts', async (req: Request, res: Response) => {
+  try {
+    const houseDistricts = await query(
+      `SELECT DISTINCT state_house_district FROM Location`
+    );
+    return res.status(OK).json(houseDistricts);
+  } catch (err) {
+    logger.error(err.message, err);
+    return res.status(BAD_REQUEST).json({
+      error: err.message,
+    });
+  }
+});
+
+/**
+ * Returns all senate districts in which gun crimes can occur.
+ */
+router.get('/senateDistricts', async (req: Request, res: Response) => {
+  try {
+    const senateDistricts = await query(
+      `SELECT DISTINCT state_senate_district FROM Location`
+    );
+    return res.status(OK).json(senateDistricts);
+  } catch (err) {
+    logger.error(err.message, err);
+    return res.status(BAD_REQUEST).json({
+      error: err.message,
+    });
+  }
+});
+
+export default router;
--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,13 +1,15 @@
 import { Router } from 'express';
-import IncidentRouter from './Incidents';
+import IncidentRouter from './Incident';
 import DeepDiveRouter from './DeepDive';
+import LocationRouter from './Location';
 
 // Init router and path
 const router = Router();
 
 // Add sub-routes
-router.use('/incidents', IncidentRouter);
+router.use('/incident', IncidentRouter);
 router.use('/deepdive', DeepDiveRouter);
+router.use('/location', LocationRouter);
 
 // Export the base-router
 export default router;
