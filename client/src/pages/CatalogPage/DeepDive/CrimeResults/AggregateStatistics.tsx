9603da409a254c86600689a8880b4349ac6c267a
AleksandrHovhannisyan <aleksandrhovhannisyan@gmail.com>
2019-11-12 19:46:58 -0500

    Refactor crime results

---
+++ client/src/pages/CatalogPage/DeepDive/CrimeResults/AggregateStatistics.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { GunCrime } from 'pages/CatalogPage/GunCrime';
+import { Statistic } from 'antd';
+import styles from './AggregateStatistics.module.less';
+
+interface AggregateStatisticsProps {
+  gunCrimes: GunCrime[];
+}
+
+const AggregateStatistics = (props: AggregateStatisticsProps) => {
+  return (
+    <section className={styles.statistics}>
+      <h2>Statistics at a Glance:</h2>
+      <div className={styles.statisticGrid}>
+        <Statistic title="Total # of crimes" value={props.gunCrimes.length} />
+        <Statistic
+          title="Total killed"
+          value={props.gunCrimes.reduce(
+            (acc, { N_KILLED }) => acc + N_KILLED,
+            0
+          )}
+        />
+        <Statistic
+          title="Total injured"
+          value={props.gunCrimes.reduce(
+            (acc, { N_INJURED }) => acc + N_INJURED,
+            0
+          )}
+        />
+        <Statistic
+          title="Total # of guns involved"
+          value={props.gunCrimes.reduce(
+            (acc, { N_GUNS_INVOLVED }) => acc + N_GUNS_INVOLVED,
+            0
+          )}
+        />
+      </div>
+    </section>
+  );
+};
+
+export default AggregateStatistics;
--- client/src/pages/CatalogPage/DeepDive/CrimeResults/CrimeResults.tsx
+++ client/src/pages/CatalogPage/DeepDive/CrimeResults/CrimeResults.tsx
@@ -10,23 +10,26 @@ import Gun from 'entityTypes/Gun';
 import Participant from 'entityTypes/Participant';
 import LoadingSpin from 'components/LoadingSpin/LoadingSpin';
 import IncidentCharacteristic from 'entityTypes/IncidentCharacteristic';
+import AggregateStatistics from './AggregateStatistics';
 
 // TODO: maybe extract the modal into its own component
 interface CrimeResultsState {
   detailsModalVisible: boolean;
   detailsModalID: number;
   detailsModalCharacteristics: IncidentCharacteristic[];
-  waitingForCharacteristicData: boolean;
   detailsModalParticipants: Participant[];
-  waitingForParticipantData: boolean;
   detailsModalGuns: Gun[];
-  waitingForGunData: boolean;
+  waitingForDetailsData: boolean;
 }
 
 interface CrimeResultsProps {
   gunCrimes: GunCrime[];
   dataFetchFailed: boolean;
 }
+
+const interpret = (field: any) => {
+  return field ? field : 'Unknown';
+};
 
 class CrimeResults extends React.Component<
   CrimeResultsProps,
@@ -38,22 +41,24 @@ class CrimeResults extends React.Component<
       detailsModalVisible: false,
       detailsModalID: -1,
       detailsModalCharacteristics: [],
-      waitingForCharacteristicData: false,
       detailsModalParticipants: [],
-      waitingForParticipantData: false,
       detailsModalGuns: [],
-      waitingForGunData: false,
+      waitingForDetailsData: false,
     };
   }
 
   private showCrimeDetails = async (id: number) => {
+    // Don't refetch the modal data if it's the one we had open just before
+    if (id === this.state.detailsModalID) {
+      this.setState({ ...this.state, detailsModalVisible: true });
+      return;
+    }
+
     this.setState({
       ...this.state,
       detailsModalID: id,
       detailsModalVisible: true,
-      waitingForCharacteristicData: true,
-      waitingForParticipantData: true,
-      waitingForGunData: true,
+      waitingForDetailsData: true,
     });
 
     try {
@@ -65,9 +70,7 @@ class CrimeResults extends React.Component<
 
       this.setState({
         ...this.state,
-        waitingForCharacteristicData: false,
-        waitingForParticipantData: false,
-        waitingForGunData: false,
+        waitingForDetailsData: false,
         detailsModalCharacteristics: characteristics.data,
         detailsModalParticipants: participants.data,
         detailsModalGuns: guns.data,
@@ -96,100 +99,67 @@ class CrimeResults extends React.Component<
     return (
       <section>
         {this.props.gunCrimes.length !== 0 ? (
-          <section className={styles.statistics}>
-            <h2>Statistics at a Glance:</h2>
-            <div className={styles.statisticGrid}>
-              <Statistic
-                title="Total # of crimes"
-                value={this.props.gunCrimes.length}
-              />
-              <Statistic
-                title="Total killed"
-                value={this.props.gunCrimes.reduce(
-                  (acc, { N_KILLED }) => acc + N_KILLED,
-                  0
-                )}
-              />
-              <Statistic
-                title="Total injured"
-                value={this.props.gunCrimes.reduce(
-                  (acc, { N_INJURED }) => acc + N_INJURED,
-                  0
-                )}
-              />
-              <Statistic
-                title="Total # of guns involved"
-                value={this.props.gunCrimes.reduce(
-                  (acc, { N_GUNS_INVOLVED }) => acc + N_GUNS_INVOLVED,
-                  0
-                )}
-              />
-            </div>
-          </section>
+          <AggregateStatistics gunCrimes={this.props.gunCrimes} />
         ) : null}
         <Modal
           visible={this.state.detailsModalVisible}
           title={`Details for incident #${this.state.detailsModalID}`}
-          destroyOnClose={true}
+          destroyOnClose={false}
           onOk={this.hideCrimeDetails}
           onCancel={this.hideCrimeDetails}
           footer={null}
         >
-          <section>
-            <h4>Characteristics</h4>
-            {this.state.waitingForCharacteristicData ? (
-              <LoadingSpin />
-            ) : (
-              this.state.detailsModalCharacteristics.map(
-                (c: IncidentCharacteristic) => (
-                  <p key={`${c.INCIDENT_ID}${c.INCIDENT_CHARACTERISTIC}`}>
-                    {c.INCIDENT_CHARACTERISTIC}
-                  </p>
-                )
-              )
-            )}
-          </section>
-          <section>
-            <h4>Participants involved:</h4>
-            {this.state.waitingForParticipantData ? (
-              <LoadingSpin />
-            ) : (
-              this.state.detailsModalParticipants.map((p: Participant) => {
-                return (
-                  <p key={`participant${p.ID}`}>
-                    Name: {p.NAME ? p.NAME : 'Unknown'}
-                    <br />
-                    Age: {p.AGE ? p.AGE : 'Unknown'}
-                    <br />
-                    Type: {p.TYPE ? p.TYPE : 'Unknown'}
-                    <br />
-                    Status: {p.STATUS ? p.STATUS : 'Unknown'}
-                    <br />
-                    Relationship: {p.RELATIONSHIP
-                      ? p.RELATIONSHIP
-                      : 'Unknown'}{' '}
-                  </p>
-                );
-              })
-            )}
-          </section>
-          <section>
-            <h4>Guns involved:</h4>
-            {this.state.waitingForGunData ? (
-              <LoadingSpin />
-            ) : (
-              this.state.detailsModalGuns.map((g: Gun) => {
-                return (
-                  <p key={`gun${g.ID}`}>
-                    Type: {g.TYPE ? g.TYPE : 'Unknown'}
-                    <br />
-                    Stolen:{' '}
-                    {g.STOLEN === 1 ? 'Yes' : g.STOLEN === 0 ? 'No' : 'Unknown'}
-                  </p>
-                );
-              })
-            )}
-          </section>
+          {this.state.waitingForDetailsData ? (
+            <LoadingSpin tip="Loading details..." />
+          ) : (
+            <>
+              <section>
+                <h4>Characteristics</h4>
+                {this.state.detailsModalCharacteristics.map(
+                  (c: IncidentCharacteristic) => (
+                    <p key={`${c.INCIDENT_ID}${c.INCIDENT_CHARACTERISTIC}`}>
+                      {c.INCIDENT_CHARACTERISTIC}
+                    </p>
+                  )
+                )}
+              </section>
+              <section>
+                <h4>Participants involved:</h4>
+                {this.state.detailsModalParticipants.map((p: Participant) => {
+                  return (
+                    <p key={`participant${p.ID}`}>
+                      Name: {interpret(p.NAME)}
+                      <br />
+                      Age: {interpret(p.AGE)}
+                      <br />
+                      Type: {interpret(p.TYPE)}
+                      <br />
+                      Status: {interpret(p.STATUS)}
+                      <br />
+                      Relationship: {interpret(p.RELATIONSHIP)}
+                    </p>
+                  );
+                })}
+              </section>
+              <section>
+                <h4>Guns involved:</h4>
+                {this.state.detailsModalGuns.map((g: Gun) => {
+                  return (
+                    <p key={`gun${g.ID}`}>
+                      Type: {interpret(g.TYPE)}
+                      <br />
+                      Stolen:{' '}
+                      {g.STOLEN === 1
+                        ? 'Yes'
+                        : g.STOLEN === 0
+                        ? 'No'
+                        : 'Unknown'}
+                    </p>
+                  );
+                })}
+              </section>
+            </>
+          )}
         </Modal>
         <List
           pagination={{ showSizeChanger: true, showQuickJumper: true }}
