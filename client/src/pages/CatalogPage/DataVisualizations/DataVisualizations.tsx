a628702cbc3a74cb68c288f158223886ef125ca7
Rachana <43420002+iracoon@users.noreply.github.com>
2019-11-16 07:18:34 -0600

    add disclaimers about data

--- client/src/pages/CatalogPage/DataVisualizations/DataVisualizations.tsx
+++ client/src/pages/CatalogPage/DataVisualizations/DataVisualizations.tsx
@@ -5,18 +5,34 @@ import LineGraph from './Graphs/LineGraphs';
 import DonutGraph from './Graphs/DonutGraphs';
 import PolarGraph from './Graphs/PolarGraphs';
 import BarGraph from './Graphs/BarGraphs';
-import { Row, Col, Button } from 'antd';
+import { Row, Col, Button, Collapse } from 'antd';
+
+const { Panel } = Collapse;
 
 class DataVisualizations extends React.Component {
   public render() {
     return (
       <div>
         <Page title={PageEnum.DATA_VISUALIZATIONS.title}>
-          <Row gutter={24}>
-            <Button type="primary" shape="round">
-              Gun Deaths Per Year
-            </Button>
-          </Row>
+          <Collapse defaultActiveKey={['1']}>
+            <Panel header="Customize Dashboard" key="1">
+              <p>(to do)</p>
+            </Panel>
+            <Panel header="Disclaimers" key="2">
+              <ul>
+                <li>
+                  The list of incidents from 2013 is not exhaustive; only 279
+                  incidents from that year were catalogued. The list of
+                  incidents only go uptil March of 2018.
+                </li>
+                <li>
+                  2 incidents (includes the Las Vegas shooting) were manually
+                  removed from the original dataset due to issues while data
+                  gathering.
+                </li>
+              </ul>
+            </Panel>
+          </Collapse>
           <br />
           <Row gutter={24}>
             <Col span={12}>
